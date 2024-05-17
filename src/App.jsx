5fe471a30ff14daac28d78899632ff360b60f402
Sherzod Tohirov <sherzod@Yaroslavs-iMac.local>
2024-05-17 16:30:09 +0500

    Initilaze 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,54 +6,50 @@ function App() {
   const mapContainerRef = useRef();
   useEffect(() => {
      mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
-     console.log(import.meta.env.VITE_MAPBOX_TOKEN)
      const map = new mapboxgl.Map({
        container: mapContainerRef.current,
-       style: 'mapbox://styles/mapbox/streets-v11',
-       center: [-74.5, 40],
-       zoom: 9
+       style: 'mapbox://styles/mapbox/satellite-streets-v11',
+       center: [69.2401, 41.2995],
+       zoom: 12,
+       pitch: 45,
+       bearing: -5.6,
      })
 
     map.on('load', () => {
-      map.addSource('polygon', {
-        type: 'geojson',
-        data: {
-          type: 'Feature',
-          properties: {},
-          geometry: {
-            type: 'Polygon',
-            coordinates: [
-              [-74.5, 40],
-              [-74.0, 40.5],
-              [-73.5, 40],
-              [-74.5, 40],
-            ],
-          },
-        },
-      });
-
+      // Add 3D buildings layer
       map.addLayer({
-        id: 'polygon',
-        type: 'fill',
-        source: 'polygon',
-        layout: {},
-        paint: {
-          'fill-color': 'red',
-          'fill-opacity': 1.5,
-        },
-      });
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+          'fill-extrusion-color': '#aaa',
 
-      map.addLayer({
-        id: 'polygon-outline',
-        type: 'line',
-        source: 'polygon',
-        layout: {},
-        paint: {
-          'line-color': 'red',
-          'line-width': 2,
-        },
-      });
-    });
+          // Use an 'interpolate' expression to add a smooth transition effect to the buildings' heights
+          'fill-extrusion-height': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'height']
+          ],
+          'fill-extrusion-base': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'min_height']
+          ],
+          'fill-extrusion-opacity': 0.6
+        }
+      })
+    })
 
     return () => {
        map.remove();
